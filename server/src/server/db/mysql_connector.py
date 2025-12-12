c4991d44dda22ee6f4e1ad2c451cf2692bab0bc4
Ryan Kleffman <ryankleffman47@gmail.com>
2025-12-12 16:17:22 +0000

    black formatting

--- server/src/server/db/mysql_connector.py
+++ server/src/server/db/mysql_connector.py
@@ -1,13 +1,10 @@
 import mysql.connector
 from ..instance import env_config
 
+
 def get_mysql_connection() -> object:
     user = env_config.get("MYSQL_ROOT_USER")
     password = env_config.get("MYSQL_ROOT_PASSWORD")
 
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user=user,
-        password=password
-    )
+    mydb = mysql.connector.connect(host="localhost", user=user, password=password)
     return mydb
