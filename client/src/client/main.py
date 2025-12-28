b0e001a84232622d1d46db8f401ff35096476133
Ryan Kleffman <ryankleffman47@gmail.com>
2025-12-28 07:56:40 +0000

    Async windows fix

--- client/src/client/main.py
+++ client/src/client/main.py
@@ -1,8 +1,7 @@
 from nicegui import app, ui
 import logging
 import client.src.client.log
-import sys
-import asyncio
+import platform
 
 # notes, just import your page here and it'll do the import python magic to add it to the web interface
 # also, use full paths due to nicegui being picky about relative paths
@@ -15,4 +14,6 @@ server_log = logging.getLogger("server")
 
 
 # ui.run(native=True, dark=True)
-ui.run(native=False, dark=True, show=False)
+ui.run(
+    native=False, dark=True, show=False, reload=platform.system() != "Windows"
+)  # reload false to disable reload, which breaks async on windows
