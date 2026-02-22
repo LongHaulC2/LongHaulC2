de8b0ecc4df6fff770a27e25b8065b4240a87e68
Ryan Kleffman <ryankleffman47@gmail.com>
2026-02-22 23:47:37 +0000

    add # noqa: F401

--- client/src/client/main.py
+++ client/src/client/main.py
@@ -1,22 +1,23 @@
 import logging
-import platform
 from pathlib import Path
 
 from nicegui import app, ui
 
-import client.src.client.log
-import client.src.client.pages.docs
-import client.src.client.pages.graph
-import client.src.client.pages.implant
-import client.src.client.pages.listeners
-import client.src.client.pages.login
+# note: # noqa: F401 ignores these in RUFF. TLDR, these need to get
+# imported to render the page/register it
+import client.src.client.log  # noqa: F401
+import client.src.client.pages.docs  # noqa: F401
+import client.src.client.pages.graph  # noqa: F401
+import client.src.client.pages.implant  # noqa: F401
+import client.src.client.pages.listeners  # noqa: F401
+import client.src.client.pages.login  # noqa: F401
 
 # notes, just import your page here and it'll do the import python magic to add it to the web interface
 # also, use full paths due to nicegui being picky about relative paths
-import client.src.client.pages.operations
-import client.src.client.pages.payloads
-import client.src.client.pages.scripts
-import client.src.client.pages.search
+import client.src.client.pages.operations  # noqa: F401
+import client.src.client.pages.payloads  # noqa: F401
+import client.src.client.pages.scripts  # noqa: F401
+import client.src.client.pages.search  # noqa: F401
 
 server_log = logging.getLogger("server")
 
@@ -28,9 +29,7 @@ app.add_static_files(
     url_path="/static",
 )
 
-ui.add_head_html(
-    '<link rel="stylesheet" type="text/css" href="/static/theme.css">', shared=True
-)
+ui.add_head_html('<link rel="stylesheet" type="text/css" href="/static/theme.css">', shared=True)
 
 # a tweak for native to allow dl's
 # https://github.com/zauberzeug/nicegui/issues/3402
@@ -38,8 +37,6 @@ app.native.settings["ALLOW_DOWNLOADS"] = True
 
 
 # ui.run(native=True, dark=True)
-ui.run(
-    native=False, dark=True, show=False, reload=False, port=8081
-)  # reload=platform.system() != "Windows")
+ui.run(native=False, dark=True, show=False, reload=False, port=8081)  # reload=platform.system() != "Windows")
 # reload false to disable reload, which breaks async on windows
 # https://github.com/zauberzeug/nicegui/issues/486
